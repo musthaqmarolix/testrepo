1b8ea7072d5996cf94becb86684021d25f603fdc
musthaq <musthaqahmed.marolix@gmail.com>
2023-10-04 15:46:34 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,43 @@
+# import time
+# from imp import reload
+# import module1
+# print("my program going to sleep")
+# time.sleep(20)
+# reload(module1)
+# import module1
+# print("the end")
+
+
+#reload(module_name)
+
+#random 
+
+#random():
+#goint to print between 0 and 1 (not inclusive)
+
+from random import *
+# for i in range(10):
+#     print(random())
+
+
+#randint()(inclusive)
+# for i in range(5):
+#     print(randint(1,50))
+
+#3.uniform()(it returns random float values between 1 to 10 and it is not inclusive)
+# for i in range(10):
+#     print(uniform(1,50))
+
+#4.randrange(start,stop,step)(start value is inclusive and stop value is not inclusive)
+#-> start value is optional , and defualt value will be taken as 0
+#->step is also optional defualt value will be 1 
+# for i in range(10):
+#     print(randint(1000,9999))
+# #1000
+#2222
+#3333
+
+# for i in range(100):
+#     print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep="")
+
+# #012345
